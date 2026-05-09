c1edd765a4d1ac84aaea9dc9772d420f59a759d2
Saarthak1234 <saarthakagarwal0408@gmail.com>
2026-05-09 18:16:48 +0530

    chore(backend): add express server setup with middleware and route mounting

---
+++ backend/server.js
@@ -0,0 +1,50 @@
+//Basic Express server setup
+import express from 'express';
+import authRoutes from './routes/authRoutes.js';
+import connectDB from './utils/db.js';
+import dotenv from 'dotenv';
+import User from './models/userModel.js';
+// import passport from './utils/passport.js';
+import session from 'express-session';
+import cors from 'cors';
+
+dotenv.config();
+connectDB();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(cors({
+    origin: "http://localhost:5173", // Your React dev server
+    credentials: true,
+}));
+
+app.use(express.json());
+
+app.use(session({
+    secret: process.env.SESSION_SECRET || 'your_session_secret',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { 
+        secure: false, // Set to true in production with HTTPS
+        httpOnly: true,
+        sameSite: 'lax',
+    }
+}));
+
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+app.get('/', (req, res) => {
+    console.log("Root endpoint hit");
+    res.send('Hello World!');
+})
+
+import leadRoutes from './routes/leadRoutes.js';
+
+app.use("/auth",authRoutes);
+app.use("/api/leads", leadRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
